1304ff44f6d42ce35f924699133ebd66532a9ecb
Santiago Caci <santiago.caci@gmail.com>
2022-11-17 18:30:00 -0300

    Add City to LocalStorage

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,11 +1,38 @@
 <script setup>
 import { ref } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+import { uid } from 'uid';
 import BaseModal from '@/components/BaseModal.vue';
 import TheInfoContent from '@/components/TheInfoContent.vue';
 
 const isModalActive = ref(null);
+const route = useRoute();
+const router = useRouter();
 
 const toggleModal = () => (isModalActive.value = !isModalActive.value);
+
+const savedCities = ref([]);
+const addCity = () => {
+  if (localStorage.getItem('savedCities')) {
+    savedCities.value = JSON.parse(localStorage.getItem('savedCities'));
+  }
+  const locationObj = {
+    id: uid(),
+    state: route.params.state,
+    city: route.params.city,
+    coords: {
+      lat: route.query.lat,
+      lon: route.query.lon,
+    },
+  };
+
+  savedCities.value.push(locationObj);
+  localStorage.setItem('savedCities', JSON.stringify(savedCities.value));
+
+  const query = { ...route.query };
+  delete query.preview;
+  router.replace({ query });
+};
 </script>
 
 <template>
@@ -22,12 +49,16 @@ const toggleModal = () => (isModalActive.value = !isModalActive.value);
         </div>
       </router-link>
 
-      <div class="flex gap-3 flex-1 justify-end">
+      <div class="flex gap-4 flex-1 justify-end">
         <i
-          class="fa-sharp fa-solid fa-circle-info text-xl cursor-pointer"
+          class="fa-sharp fa-solid fa-circle-info text-2xl cursor-pointer"
           @click="toggleModal"
         ></i>
-        <i class="fa-solid fa-plus text-xl cursor-pointer"></i>
+        <i
+          v-if="route.query.preview"
+          @click="addCity"
+          class="fa-solid fa-plus text-2xl cursor-pointer"
+        ></i>
       </div>
 
       <BaseModal
